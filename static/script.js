c1033fa673aab610cc6503acd970065a68b1b035
mohitkumar <mohitbihari3677788@gmail.com>
2026-09-18 23:43:29 +0530

    add about and home

--- static/script.js
+++ static/script.js
@@ -1068,3 +1068,27 @@ function getWindDirection(degrees) {
 
     return directions[index];
 }
+
+
+
+
+
+
+/* ========================================
+   NAVBAR SCROLL EFFECT
+======================================== */
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+
+    if (!navbar) return;
+
+    if (window.scrollY > 30) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
+ 
